7b2904bd922cf274d6059e471834baab085c78e7
Vishal Kaushik <vishalkaushik2002@gmail.com>
2023-11-01 14:21:25 +0530

    Updated api/admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Stone, Activation
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Stone)
+admin.site.register(Activation)
